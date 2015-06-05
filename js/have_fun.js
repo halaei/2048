8f7e9346465298b786b5f43d3835ef92ef17035c
Hamid Alaei V <hamid.a85@gmail.com>
2015-06-05 22:45:31 +0430

    getting rid of canvas_controller

--- js/have_fun.js
+++ js/have_fun.js
@@ -5,7 +5,6 @@ function haveFun()
 {
 	var game_div = document.getElementById("Triangular2048MainDiv");
     var view = game_div.getElementsByClassName("canvas_view")[0];
-    var controller = game_div.getElementsByClassName("canvas_controller")[0];
     var reset = game_div.getElementsByClassName("restart-button")[0];
 
     var score = game_div.getElementsByClassName("score")[0];
@@ -14,7 +13,7 @@ function haveFun()
 	return new Game(
 		new Grid(4),
 		new StatusLog(storage),
-		[new CanvasController(controller), new KeyboardController(document), new TouchController(view), new Reset(reset)],
+		[new KeyboardController(document), new TouchController(view), new Reset(reset)],
 		new CanvasView(view, 4),
         new Scorboard(score, highscore, 0, storage)
     );
